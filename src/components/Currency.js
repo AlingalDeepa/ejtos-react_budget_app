373ac629b6e32c538acd02ee6ca70f5b0636de2d
Deepa Alingal <deepaalingal922@gmail.com>
2024-01-06 11:06:18 -0500

    Currency updates

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,25 +2,45 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 import  '../component.css';
 
+const increaseAllocation = (name) => {
+    const expense = {
+        name: name,
+        cost: 10,
+    };
+
+    dispatch({
+        type: 'ADD_EXPENSE',
+        payload: expense
+    });
+
+}
+
 
 const Currency = () => {
     const { currency } = useContext(AppContext);
     const [newCurrency, setNewCurrency] = useState(currency);
     const handleCurrencyChange = (event) => {
+        /*console.log("Console"+event.target.value)*/
         setNewCurrency(event.target.value);
+        /*currency = newCurrency;*/
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: newCurrency,
+           
+        });
     }
     return (
 <div  style={{background: "#B7EBC6"}} >
 
             <label htmlFor="inputGroupSelect01" style={{color: "#5BA571"}}>Currency: </label>
                   
-<select className="select-container" id="inputGroupSelect01" >
-    <option value="Marketing" name="marketing" className="select-option">$ Dollar</option>
-    <option value="Sales" name="sales" className="select-option">£ Pound</option>
-    <option value="Finance" name="finance" className="select-option">&#x20AC; Euro</option>
-    <option value="HR" name="hr" className="select-option">&#x20B9; Ruppee</option>
+<select className="select-container" id="inputGroupSelect01" onChange={handleCurrencyChange}>
+    <option value="$ Dollar" name="Dollar" className="select-option" >$ Dollar</option>
+    <option value="Pound" name="Pound" className="select-option">£ Pound</option>
+    <option value="Euro" name="Euro" className="select-option">&#x20AC; Euro</option>
+    <option value="Ruppee" name="Ruppee" className="select-option">&#x20B9; Ruppee</option>
 </select>
-
+<h1>{newCurrency}</h1>
 </div>
 
 
